436525df6916aa8ae7c777548dbf469617e0d9c0
kubatbaew <kubatbaew@gmail.com>
2023-10-23 17:40:27 +0600

    settings project

--- onlinemarket/settings.py
+++ onlinemarket/settings.py
@@ -104,5 +104,8 @@ STATIC_URL = 'static/'
 STATIC_DIR = BASE_DIR / 'static'
 STATICFILES_DIRS = [STATIC_DIR]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
